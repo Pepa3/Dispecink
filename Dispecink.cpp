3fa3a5d22452bfaac597d16107ed26c1e84fe85b
Pepa3 <svobodajosef117@gmail.com>
2024-10-09 15:18:32 +0200

    better curves, load tracks from file

--- Dispecink.cpp
+++ Dispecink.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 #include <cmath>
+#include <fstream>
 
 const double PI = 3.1415926;
 void drawArc(sf::RenderWindow* window, int x, int y, int r, double start, double len){
@@ -13,20 +14,21 @@ void drawArc(sf::RenderWindow* window, int x, int y, int r, double start, double
 
 const int wwidth = 800, wheight = 600;
 int offsetX = 400, offsetY = 300;
-int scale = 50;
+int scale = 30;
 int counter = 1;
 class Track{
 public:
     enum Type{STRAIGHT=1,CURVE=2};
-    int width, height;
+    int width, height, mod;
     int startId;
     Type type;
     Track(){}
-    Track(Type type, int width, int height, int startId){
+    Track(Type type, int width, int height, int startId, int mod = 0){
         this->type = type;
         this->width = width;
         this->height = height;
         this->startId = startId;
+        this->mod = mod;
     }
 };
 
@@ -41,10 +43,25 @@ int main(){
     sf::RenderWindow window(sf::VideoMode(wwidth, wheight), "Dispecink");
     window.setFramerateLimit(30);
     tracks = std::map<int,Track>();
-    tracks[0] = Track(Track::Type::STRAIGHT, 3, 0, -1);//node 0
-    tracks[1] = Track(Track::Type::CURVE, 3, 3, 0);
-    tracks[2] = Track(Track::Type::STRAIGHT, 0, 3, 0);
+    tracks[0] = Track(Track::Type::STRAIGHT, 0, 0, -1);//node 0
 
+    std::ifstream in = std::ifstream("stanice");
+    while(!in.eof() && !in.bad()){
+        int id;
+        Track t;
+        in >> id >> t.startId >> (int&)t.type >> t.width >> t.height;
+        if(t.type == Track::Type::CURVE) in >> t.mod;
+        tracks[id] = t;
+    }
+   /* tracks[1] = Track(Track::Type::STRAIGHT, 3, 0, 0);
+    tracks[2] = Track(Track::Type::STRAIGHT, -3, 0, 0);
+    tracks[3] = Track(Track::Type::STRAIGHT, 0, 3, 0);
+    tracks[4] = Track(Track::Type::STRAIGHT, 0, -3, 0);
+    tracks[5] = Track(Track::Type::CURVE, 3, 3, 1, 0);
+    tracks[6] = Track(Track::Type::CURVE, -3, -3, 2, 0);
+    tracks[7] = Track(Track::Type::CURVE, -3, 3, 3, 1);
+    tracks[8] = Track(Track::Type::CURVE, 3, -3, 4, 1);
+    */
     while(window.isOpen()){
         sf::Event event;
         while(window.pollEvent(event)){
@@ -75,7 +92,7 @@ int main(){
                 sf::Vertex vert[] = {sf::Vertex(sf::Vector2f(x,y)),sf::Vertex(sf::Vector2f(x+t.second.width*scale,y+t.second.height*scale))};
                 window.draw(vert, 2, sf::LineStrip);
             }else if(t.second.type == Track::Type::CURVE){
-                drawArc(&window, x, y - t.second.height * scale, t.second.width * scale, 90, 90);
+                drawArc(&window, x - (t.second.mod == 1 ? t.second.width * scale : 0), y - (t.second.mod==0?t.second.height * scale : 0), t.second.width * scale, t.second.height*t.second.width>0 ? 90 : 0, 90);
             }
         }
         //drawArc(&window, 400, 300, 100, 0, 180);
